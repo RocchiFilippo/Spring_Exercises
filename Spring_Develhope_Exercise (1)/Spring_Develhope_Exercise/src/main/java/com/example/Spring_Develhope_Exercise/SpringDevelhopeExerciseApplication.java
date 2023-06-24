68f8c8982d1e0f55afa9a51f6039b674009ae202
FilippoRocchi <filipporocchi00@gmail.com>
2023-06-24 16:14:13 +0200

    Spring_Exercises_Inizio

---
+++ Spring_Develhope_Exercise (1)/Spring_Develhope_Exercise/src/main/java/com/example/Spring_Develhope_Exercise/SpringDevelhopeExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Develhope_Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevelhopeExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevelhopeExerciseApplication.class, args);
+	}
+
+}
